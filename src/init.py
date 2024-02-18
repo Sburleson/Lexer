eb525da0e5dabedadbf5600cd6d322ddaf4d7516
Sburleson <108891820+Sburleson@users.noreply.github.com>
2024-02-18 12:48:02 -0500

    Update init.py

--- src/init.py
+++ src/init.py
@@ -1,1 +1,40 @@
+from lex import lex
+from yacc import yacc
+
+# All tokens must be named in advance.
+tokens = ( 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'LPAREN', 'RPAREN',
+           'NAME', 'NUMBER','AND','OR','SORT','LBRA','RBRA','EQUAL',
+            'COM','QUOTE','PERIOD','SCOLN' )
+
+t_ignore = ' \t'
+
+t_PLUS= r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+t_NUMBER= r'\d+'
+t_AND=r'\&'
+t_OR=r'\|'
+t_SORT= r'\>'
+t_LBRA=r'\['
+t_RBRA=r'\]'
+t_COM=r'\,'
+t_QUOTE=r'\"|\''
+t_PERIOD=r'\.'
+t_SCOLN=r'\;'
+
+
+def t_NUMBER(t):
+    r'\+'
+    t.value = int(t.value)
+    return t
+
+def t_error(t):
+    print(f'Illegal character {t.value[0]!r}')
+    t.lexer.skip(1)
+
+lexer = lex()
 
