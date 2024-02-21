fc4b4f455a73361706d7f8a139447cc8924302bb
MelissaPatton <pattonm1@etown.edu>
2024-02-21 13:50:45 -0500

    parser

--- src/init.py
+++ src/init.py
@@ -14,8 +14,8 @@ t_TIMES = r'\*'
 t_DIVIDE = r'/'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
-##t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
-##t_NUMBER= r'\d+'
+t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+t_NUMBER= r'\d+'
 t_AND=r'\&'
 t_OR=r'\|'
 t_SORT= r'\>'
@@ -26,16 +26,6 @@ t_QUOTE=r'\"|\''
 t_PERIOD=r'\.'
 t_SCOLN=r'\;'
 
-
-def t_NUMBER(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
-
-def t_NAME(t):
-    r'[a-zA-Z_][a-zA-Z0-9_]*'
-    return t
-
 def t_error(t):
     print(f'Illegal character {t.value[0]!r}')
     t.lexer.skip(1)
@@ -43,7 +33,7 @@ def t_error(t):
 lexer = lex()
 
 
-data = "3 + 4"
+data = " 3+ 6"
 
 # Give the lexer some input
 lexer.input(data)
@@ -54,4 +44,69 @@ while True:
     if not tok:
         break  # No more input
     print(tok)
-    
+
+def p_expression(p):
+    '''
+    expression : term PLUS term
+               | term MINUS term
+    '''
+    # p is a sequence that represents rule contents.
+    #
+    # expression : term PLUS term
+    #   p[0]     : p[1] p[2] p[3]
+    # 
+    p[0] = ('binop', p[2], p[1], p[3])
+
+def p_expression_term(p):
+    '''
+    expression : term
+    '''
+    p[0] = p[1]
+
+def p_term(p):
+    '''
+    term : factor TIMES factor
+         | factor DIVIDE factor
+    '''
+    p[0] = ('binop', p[2], p[1], p[3])
+
+def p_term_factor(p):
+    '''
+    term : factor
+    '''
+    p[0] = p[1]
+
+def p_factor_number(p):
+    '''
+    factor : NUMBER
+    '''
+    p[0] = ('number', p[1])
+
+def p_factor_name(p):
+    '''
+    factor : NAME
+    '''
+    p[0] = ('name', p[1])
+
+def p_factor_unary(p):
+    '''
+    factor : PLUS factor
+           | MINUS factor
+    '''
+    p[0] = ('unary', p[1], p[2])
+
+def p_factor_grouped(p):
+    '''
+    factor : LPAREN expression RPAREN
+    '''
+    p[0] = ('grouped', p[2])
+
+def p_error(p):
+    print(f'Syntax error at {p.value!r}')
+
+# Build the parser
+parser = yacc()
+
+# Parse an expression
+ast = parser.parse('2 * 3 + 4 * (5 - x)')
+print(ast)
