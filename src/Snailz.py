57d256baee1b362ba7d3cd0b8eea46a8c58aea57
MelissaPatton <pattonm1@etown.edu>
2024-04-03 19:08:18 -0400

    implemented AST

--- src/Snailz.py
+++ src/Snailz.py
@@ -2,6 +2,11 @@ from ply import lex
 from ply import yacc
 import os
 
+class ASTNode:
+    def __init__(self, type, children=None, value=None):
+        self.type = type
+        self.children = children if children else []
+        self.value = value
 
 class Parser:
     """
@@ -35,36 +40,40 @@ class Parser:
                 break
             if not s:
                 continue
-            yacc.parse(s)
+            self.parse_result = yacc.parse(s)
+            if self.parse_result:
+                self.print_ast(self.parse_result)
+
+
 class Snailz(Parser):
     
-    tokens =  ( 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'LPAREN', 'RPAREN',
-           'NAME', 'NUMBER','AND','OR','GR8R','LBRA','RBRA','COM', 'QUOTE',
-           'PERIOD','SCOLN','COMPEQU','EQUALS', 'LES', 'MOD', 'SORT',
-            'NOT', 'VAR','EXP')
+    tokens =  ('PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'LPAREN', 'RPAREN',
+           'NAME', 'NUMBER', 'AND', 'OR', 'GR8R', 'LBRA', 'RBRA', 'COM', 'QUOTE',
+           'PERIOD', 'SCOLN', 'COMPEQU', 'EQUALS', 'LES', 'MOD', 'SORT',
+           'NOT', 'VAR', 'EXP')
    
-    t_PLUS= r'\+'
+    t_PLUS = r'\+'
     t_MINUS = r'-'
     t_TIMES = r'\*'
     t_DIVIDE = r'/'
     t_LPAREN = r'\('
     t_RPAREN = r'\)'
     t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
-    t_AND=r'\&'
-    t_OR=r'\|'
-    t_GR8R= r'\>'
-    t_LBRA=r'\['
-    t_RBRA=r'\]'
-    t_COM=r'\,'
-    t_QUOTE=r'\"|\''
-    t_PERIOD=r'\.'
-    t_SCOLN=r'\;'
-    t_COMPEQU=r'\=='
-    t_EQUALS=r'\='
-    t_LES=r'\<'
-    t_MOD=r'\%'
-    t_SORT=r'\>>'
-    t_NOT=r'\!'
+    t_AND = r'\&'
+    t_OR = r'\|'
+    t_GR8R = r'\>'
+    t_LBRA = r'\['
+    t_RBRA = r'\]'
+    t_COM = r'\,'
+    t_QUOTE = r'\"|\''
+    t_PERIOD = r'\.'
+    t_SCOLN = r'\;'
+    t_COMPEQU = r'\=='
+    t_EQUALS = r'\='
+    t_LES = r'\<'
+    t_MOD = r'\%'
+    t_SORT = r'\>>'
+    t_NOT = r'\!'
     t_VAR = r'[\w]+'
     t_EXP = r'\^'
 
@@ -99,11 +108,11 @@ class Snailz(Parser):
 
     def p_statement_assign(self, p):
         'statement : NAME EQUALS expression'
-        self.names[p[1]] = p[3]
+        p[0] = ASTNode('assignment', [ASTNode('variable', value=p[1]), p[3]])
 
     def p_statement_expr(self, p):
         'statement : expression'
-        print(p[1])
+        p[0] = p[1]
 
     def p_expression_binop(self, p):
         """
@@ -113,21 +122,11 @@ class Snailz(Parser):
                   | expression DIVIDE expression
                   | expression EXP expression
         """
-        # print [repr(p[i]) for i in range(0,4)]
-        if p[2] == '+':
-            p[0] = p[1] + p[3]
-        elif p[2] == '-':
-            p[0] = p[1] - p[3]
-        elif p[2] == '*':
-            p[0] = p[1] * p[3]
-        elif p[2] == '/':
-            p[0] = p[1] / p[3]
-        elif p[2] == '**':
-            p[0] = p[1] ** p[3]
+        p[0] = ASTNode(p[2], [p[1], p[3]])
 
     def p_expression_uminus(self, p):
         'expression : MINUS expression %prec UMINUS'
-        p[0] = -p[2]
+        p[0] = ASTNode('uminus', [p[2]])
 
     def p_expression_group(self, p):
         'expression : LPAREN expression RPAREN'
@@ -135,15 +134,11 @@ class Snailz(Parser):
 
     def p_expression_number(self, p):
         'expression : NUMBER'
-        p[0] = p[1]
+        p[0] = ASTNode('number', value=p[1])
 
     def p_expression_name(self, p):
         'expression : NAME'
-        try:
-            p[0] = self.names[p[1]]
-        except LookupError:
-            print("Undefined name '%s'" % p[1])
-            p[0] = 0
+        p[0] = ASTNode('variable', value=p[1])
 
     def p_error(self, p):
         if p:
@@ -151,6 +146,18 @@ class Snailz(Parser):
         else:
             print("Syntax error at EOF")
 
+    # Method to print the AST
+    def print_ast(self, node, indent=0):
+        print(' ' * indent + node.type)
+        if node.value is not None:
+            print(' ' * (indent + 2) + str(node.value))
+        for child in node.children:
+            self.print_ast(child, indent + 2)
+
 if __name__ == '__main__':
     snailz = Snailz()
     snailz.run()
+
+    # After parsing, print the AST
+    ast_root = snailz.parse_result
+    snailz.print_ast(ast_root)
